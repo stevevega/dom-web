167e85af99ad3655da0aa4f5386d1867d846ed5b
Steve Vega <vega.saborio@gmail.com>
2017-09-20 15:19:41 -0600

    Updating codeship config.

--- deploy.sh
+++ deploy.sh
@@ -15,21 +15,17 @@ sudo docker service ls | grep -q " dom-web "
 if [ $? -eq 1 ]; then
     sudo docker service create \
         --container-label version="$version" \
-        --detach=false \
         --name dom-web \
         --network dom \
         --publish 80:8080 \
-        --update-order 'start-first' \
         --replicas 4 \
         stevevega/dom-web:$tag \
     || { echo $0: Failed to create dom-web; exit 1; }
 else
     sudo docker service update \
         --container-label-add version="$version" \
-        --detach=false \
         --force \
         --image stevevega/dom-web:$tag \
-        --update-order 'start-first' \
         dom-web \
     || { echo $0: Failed to update dom-web; exit 1; }
 fi
